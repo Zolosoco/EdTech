4a25f8c0ae1ced8472c7e5f345b44ac51fc862df
Zolosoco <116631854+Zolosoco@users.noreply.github.com>
2024-12-31 07:52:53 +0530

    db Schema created

---
+++ models/db.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema ;
+const ObjectId = mongoose.Types.ObjectId;
+
+
+
+const User = new Schema ({
+    email : {type : String, unique: true},
+    name : String,
+    password : String,
+    firstName : String,
+    lastName : String,
+    purchasedCourses : [{
+        type : String,
+        ref : 'Course'
+    }]
+
+});
+
+
+const Admin = new Schema ({
+    email : {type : String, unique : true},
+    password : String,
+    firstName : String,
+    lastName : String,
+    myCourses : [{
+        type : String,
+        ref : 'Course'
+    }]
+});
+
+
+
+const Course = new Schema({
+    title : String,
+    description : String,
+    price : Number,
+    imageUrl : String
+});
+
+
+
+//models of the schema to implement functional methods;
+
+const userModel = mongoose.model('user', User);
+const adminModel = mongoose.model('admin', Admin);
+const courseModel = mongoose.model('course',Course);
+
+//exporting the models:
+
+module.exports = ({
+    userModel,
+    adminModel,
+    courseModel
+});
+
